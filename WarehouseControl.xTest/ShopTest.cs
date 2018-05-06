c7cf16e368c3f7d6da2918073fae7fffb994d8d8
Зылевич Вадим <01vadim10@gmail.net>
2018-05-06 20:19:46 +0300

    First commit

---
+++ WarehouseControl.xTest/ShopTest.cs
@@ -0,0 +1,31 @@
+﻿using WarehouseControl.Models;
+using Xunit;
+
+namespace WarehouseControl.xTest
+{
+	public class ShopTest
+	{
+		private Shop _shop;
+
+		public ShopTest()
+		{
+			_shop = new Shop();
+		}
+
+		[Fact]
+		public void CreateShop()
+		{
+			var name = "FamousTestShop1";
+			var address = "Boston, SomeTestStr st., 7 b.";
+			var shopFormat = "";
+
+			Assert.True(_shop.Create(name, address, shopFormat));
+		}
+
+		[Fact]
+		public void ReadShop()
+		{
+
+		}
+	}
+}
---
+++ WarehouseControl/Models/Shop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseControl.Models
+{
+    public class Shop
+    {
+		public Shop()
+		{
+		}
+
+		public bool Create(string name, string addr, string format)
+	    {
+		    return false;
+	    }
+    }
+}
