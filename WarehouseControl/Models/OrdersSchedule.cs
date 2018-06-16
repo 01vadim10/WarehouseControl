9d7025a2348897e8ff35da765299fbeebfb9e05b
Зылевич Вадим <01vadim10@gmail.net>
2018-06-16 15:09:50 +0300

    Orders edited

---
+++ WarehouseControl/Models/OrdersSchedule.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace WarehouseControl.Models
+{
+    public class OrdersSchedule
+    {
+        public Supplier Supplier { get; set; }
+        public string Contract { get; set; }
+        public int Gap { get; set; }
+
+    }
+}
